63dfb6d5d88f32a996c8b337e6d8f68b9dbe8e5b
Gui0liveira <143449544+Gui0liveira@users.noreply.github.com>
2023-09-08 16:32:00 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  fill ("green");
+    
+    rect(0,0,100,200);
+    
+    fill("white");
+    
+    rect(100,0,100,200);
+    
+    fill("red");
+    
+    rect(200,0,100,200);
+  }
